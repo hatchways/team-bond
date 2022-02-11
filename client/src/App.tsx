00eb9c7760551856846b5416b18aad229140694d
JobanjeetSingh <46836044+JobanjeetSingh@users.noreply.github.com>
2022-02-11 13:28:02 -0800

    Rework-routing done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,16 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './themes/theme';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Signup from './pages/SignUp/SignUp';
-import Dashboard from './pages/Dashboard/Dashboard';
 import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
 import { Navbar } from './components/Navbar/Navbar';
-import Settings from './pages/Settings/Settings';
-import NotFound from './pages/NotFound/NotFound';
-import ProfileDetail from './pages/Profile/ProfileDetail.tsx/ProfileDetail';
+import { allRoutes } from './routes/app';
 
 function App(): JSX.Element {
+  const routes = allRoutes.map((route) => {
+    return <Route key={route.route} path={route.route} component={route.page} />;
+  });
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -24,16 +22,7 @@ function App(): JSX.Element {
             <SocketProvider>
               <CssBaseline />
               <Navbar />
-              <Switch>
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route path="/profile/settings" component={Settings} />
-                <Route path="/profile/:id" component={ProfileDetail} />
-                <Route path="*">
-                  <NotFound />
-                </Route>
-              </Switch>
+              <Switch>{routes}</Switch>
             </SocketProvider>
           </AuthProvider>
         </SnackBarProvider>

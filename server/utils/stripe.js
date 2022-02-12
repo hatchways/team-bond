e7bbd1d7b2378af33737e947235ffe29064b2ab6
ajkatom <contact@alonadelson.com>
2022-02-12 12:21:39 -0500

    added product,plan,and subscription methods

--- server/utils/stripe.js
+++ server/utils/stripe.js
@@ -20,9 +20,50 @@ exports.chargeCustomer = async (id) => {
   const diff = Math.abs(startDate - endDate);
   const hours = diff / 36e5;
   const amount = (hours * sitter.rate).toFixed(2);
+  const CURRENCY = 'usd';
   stripe.charges.create({
     amount: amount + 5,
-    currency: 'usd',
+    currency: CURRENCY,
     customer: id,
   });
+};
+
+exports.createProduct = async () => {
+  const PRODUCT_NAME = 'Monthly Subscription';
+  const PRODUCT_TYPE = 'service';
+
+  const product = await stripe.products.create({
+    name: PRODUCT_NAME,
+    type: PRODUCT_TYPE,
+  });
+
+  console.log(product);
+  return product.id;
+};
+exports.createPlan = async (productId) => {
+  const PLAN_NICKNAME = 'Monthly Subscription Plan';
+  const PLAN_INTERVAL = 'month';
+  const CURRENCY = 'usd';
+  const PLAN_PRICE = 350; //just a number until we can decide on a plan number
+
+  const plan = await stripe.plans.create({
+    product: productId,
+    nickname: PLAN_NICKNAME,
+    currency: CURRENCY,
+    interval: PLAN_INTERVAL,
+    amount: PLAN_PRICE,
+  });
+
+  console.log(plan);
+  return plan.id;
+};
+
+exports.subscribeCustomerToPlan = async (customerId, planId) => {
+  const subscription = await stripe.subscriptions.create({
+    customer: customerId,
+    items: [{ plan: planId }],
+  });
+
+  console.log(subscription);
+  return subscription;
 };

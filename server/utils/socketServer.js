eccabc57da18e5aaf9781cc2f1d970b9fecf7e67
ajkatom <contact@alonadelson.com>
2022-02-16 01:38:34 -0500

    added logged out notification and map of active users

--- server/utils/socketServer.js
+++ server/utils/socketServer.js
@@ -1,17 +1,37 @@
 const jwt = require("jsonwebtoken");
 const User = require('../models/User');
+const onlineUsers = new Map();
 
-const sock = (io)=>{
-  io.on('connection',async(socket)=>{
-    socket.on('authenticate',async ()=>{
-      const setCookie = socket.handshake.headers.cookie;
-      const tempArryOne = setCookie.split('=');
+const getId = (handshake) => {
+      const tempArryOne = handshake.split('=');
       const tempArryTwo = tempArryOne[1].split(';');
       const token = tempArryTwo[0];
       const id = jwt.verify(token, process.env.JWT_SECRET).id;
-     if(await User.findById(id)) console.log('user authenticated');
+      return id
+}
+
+const sock = (io) => {
+  io.on('connection',async(socket)=>{
+    socket.on('authenticate',async ()=>{
+      const handshake = socket.handshake.headers.cookie;
+      const id = getId(handshake);
+      const user = await User.findById(id)
+     if(user) {
+       console.log(`user ${user.name} is authenticated`);
+       onlineUsers.set(id,user);
+       console.log(onlineUsers);
+       
+      }
     });
     console.log("connected");
+    socket.on('logout',async() => {
+      const handshake = socket.handshake.headers.cookie;
+      const id = getId(handshake);
+      const user = await User.findById(id)
+      onlineUsers.delete(id);
+      console.log(onlineUsers);
+      console.log(`user ${user.name} has logged out`);
+    })
   });
 }
 

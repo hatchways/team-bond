a4e284efe8bca26f37abdde7a755f54a614cb935
ajkatom <contact@alonadelson.com>
2022-02-16 21:15:39 -0500

    notification Schema done

---
+++ server/models/Notification.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const notificationSchema  = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type:String,
+    required: true,
+  },
+  description: {
+    type:String,
+    required: true,
+  },
+  read:{
+    type:Boolean,
+  },
+  date: {
+    type:Date,
+    required: true,
+  }
+
+});
+
+module.exports = Notification = mongoose.model("Notification", notificationSchema);

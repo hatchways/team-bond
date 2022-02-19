212009af4549dd60bfdaaf520b8d0ef227be8d9a
ajkatom <contact@alonadelson.com>
2022-02-18 22:57:41 -0500

    added notification templates

---
+++ server/templates/notificationTemplates.js
@@ -0,0 +1,32 @@
+const notificationContent = new Map();
+
+const bookingRequested = {
+  title: 'Booking requested',
+  description: '',
+  read: false,
+  date: new Date(),
+};
+
+const bookingAccepted = {
+  title: 'Booking accepted',
+  description: 'The sitter has accepted your booking request',
+  read: false,
+  date: new Date(),
+};
+
+const bookingDeclined = {
+  title: 'Booking declined',
+  description: 'The sitter has declined your booking request',
+  read: false,
+  date: new Date(),
+};
+const bookingPayed = {
+  title: 'Booking payed',
+  description: '',
+  read: false,
+  date: new Date(),
+};
+
+notificationContent.set([['accepted', bookingAccepted], ['declined', bookingDeclined], ['requested', bookingRequested], ['payed', bookingPayed]]);
+
+module.exports = notificationContent;

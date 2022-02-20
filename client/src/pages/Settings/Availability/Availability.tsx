c55e399548534cc5403f0ecce093100ba6812b12
ajkatom <contact@alonadelson.com>
2022-02-20 09:29:29 -0500

    working on checkboxes

--- client/src/pages/Settings/Availability/Availability.tsx
+++ client/src/pages/Settings/Availability/Availability.tsx
@@ -1,10 +1,10 @@
 import DropDownItem from './DropDownItem';
-const dropDown: JSX.Element[] = [];
-for (let i = 1; i <= 8; i++) {
-  dropDown.push(<DropDownItem key={i} />);
-}
+// const dropDown: JSX.Element[] = [];
+// for (let i = 1; i <= 8; i++) {
+//   dropDown.push(<DropDownItem key={i} />);
+// }
 function Availability(): JSX.Element {
-  return <div>{dropDown}</div>;
+  return <DropDownItem />;
 }
 
 export default Availability;
--- client/src/pages/Settings/Availability/DropDownItem.tsx
+++ client/src/pages/Settings/Availability/DropDownItem.tsx
@@ -4,8 +4,9 @@ import React from 'react';
 const initialValues = {
   startTime: '',
   endTime: '',
+  checked: [],
 };
-
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday,', 'Friday', 'Saturday'];
 const options: { value: string; label: string }[] = [];
 for (let i = 0; i < 25; i++) {
   i < 10
@@ -15,43 +16,53 @@ for (let i = 0; i < 25; i++) {
 
 function DropDownItem(): JSX.Element {
   return (
-    <>
-      <Formik
-        onSubmit={(values) =>
-          values.startTime >= values.endTime
-            ? alert('start time must be earlier then end time')
-            : alert('availability set')
-        }
-        initialValues={{ ...initialValues }}
-        render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="startTime">
-              <span>from</span>
-              <Field name="startTime" component="select">
-                {options.map((option, i) => {
-                  return (
-                    <option key={i} value={option.value}>
-                      {option.label}
-                    </option>
-                  );
-                })}
-              </Field>
-              <span>to</span>
-              <Field name="endTime" component="select">
-                {options.map((option, i) => {
-                  return (
-                    <option key={i} value={option.value}>
-                      {option.label}
-                    </option>
-                  );
-                })}
-              </Field>
-            </label>
-            <button type="submit">Submit</button>
-          </form>
-        )}
-      ></Formik>
-    </>
+    <Formik
+      onSubmit={(values) =>
+        values.startTime >= values.endTime
+          ? alert('start time must be earlier then end time')
+          : alert('availability set')
+      }
+      initialValues={{ ...initialValues }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          {days.map((day, i) => {
+            return (
+              <>
+                <label key={i}>
+                  <Field type="checkbox" name="days" />
+                  {day}
+                </label>
+                <label htmlFor="available">
+                  <span>from</span>
+                  <Field name="startTime" component="select">
+                    {options.map((option, i) => {
+                      return (
+                        <option key={i} value={option.value}>
+                          {option.label}
+                        </option>
+                      );
+                    })}
+                  </Field>
+                  <span>to</span>
+                  <Field name="endTime" component="select">
+                    {options.map((option, i) => {
+                      return (
+                        <>
+                          <option key={i} value={option.value}>
+                            {option.label}
+                          </option>
+                        </>
+                      );
+                    })}
+                  </Field>
+                </label>
+                <button type="submit">Submit</button>
+              </>
+            );
+          })}
+        </form>
+      )}
+    ></Formik>
   );
 }
 

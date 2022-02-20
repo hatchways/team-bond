6286353f264f42c10920a28fd223a1678997e5a1
ajkatom <contact@alonadelson.com>
2022-02-20 16:09:09 -0500

    got the basic design and some functionality

--- client/src/pages/Settings/Availability/DropDownItem.tsx
+++ client/src/pages/Settings/Availability/DropDownItem.tsx
@@ -1,11 +1,14 @@
 import { Formik, Field } from 'formik';
-import React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ro from 'date-fns/esm/locale/ro/index.js';
 
-const initialValues = {
-  startTime: '',
-  endTime: '',
-  checked: [],
-};
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday,', 'Friday', 'Saturday'];
 const options: { value: string; label: string }[] = [];
 for (let i = 0; i < 25; i++) {
@@ -14,52 +17,75 @@ for (let i = 0; i < 25; i++) {
     : options.push({ value: `${i}:00`, label: `${i}:00` });
 }
 
+const rows = days.map((day, i) => {
+  const row = {
+    day: day,
+    from: (
+      <Field name="to" component="select">
+        {options.map((option, i) => {
+          return (
+            <option key={i} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </Field>
+    ),
+    to: (
+      <Field name="from" component="select">
+        {options.map((option, i) => {
+          return (
+            <option key={i} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </Field>
+    ),
+  };
+  return row;
+});
+
+const initialValues = {
+  startTime: '',
+  endTime: '',
+};
+
 function DropDownItem(): JSX.Element {
+  console.log('yyyy', rows);
   return (
     <Formik
-      onSubmit={(values) =>
+      onSubmit={(values) => {
+        console.log(values);
         values.startTime >= values.endTime
           ? alert('start time must be earlier then end time')
-          : alert('availability set')
-      }
+          : alert('availability set');
+      }}
       initialValues={{ ...initialValues }}
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
-          {days.map((day, i) => {
-            return (
-              <>
-                <label key={i}>
-                  <Field type="checkbox" name="days" />
-                  {day}
-                </label>
-                <label htmlFor="available">
-                  <span>from</span>
-                  <Field name="startTime" component="select">
-                    {options.map((option, i) => {
-                      return (
-                        <option key={i} value={option.value}>
-                          {option.label}
-                        </option>
-                      );
-                    })}
-                  </Field>
-                  <span>to</span>
-                  <Field name="endTime" component="select">
-                    {options.map((option, i) => {
-                      return (
-                        <>
-                          <option key={i} value={option.value}>
-                            {option.label}
-                          </option>
-                        </>
-                      );
-                    })}
-                  </Field>
-                </label>
-                <button type="submit">Submit</button>
-              </>
-            );
-          })}
+          <label htmlFor="available">
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 450 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">Days</TableCell>
+                    <TableCell align="right">From</TableCell>
+                    <TableCell align="right">To</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row, i) => (
+                    <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell align="right">{row.day}</TableCell>
+                      <TableCell align="right">{row.from}</TableCell>
+                      <TableCell align="right">{row.to}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </label>
         </form>
       )}
     ></Formik>
@@ -67,3 +93,44 @@ function DropDownItem(): JSX.Element {
 }
 
 export default DropDownItem;
+//       <form onSubmit={handleSubmit}>
+//           {days.map((day, i) => {
+//             return (
+//               <Grid container key={i}>
+//                 <Grid item key={i} sx={{ padding: '10px' }}>
+//                   <label key={i}>
+//                     <Field type="checkbox" name={day} />
+//                     {day}
+//                   </label>
+//                   <label htmlFor="available">
+//                     <Typography component="span" sx={{ paddingX: '10px' }}>
+//                       from
+//                     </Typography>
+//                     <Field name={i} component="select">
+//                       {options.map((option, i) => {
+//                         return (
+//                           <option key={i} value={option.value}>
+//                             {option.label}
+//                           </option>
+//                         );
+//                       })}
+//                     </Field>
+//                     <span>to</span>
+//                     <Field name={options[i].value} component="select">
+//                       {options.map((option, i) => {
+//                         return (
+//                           <>
+//                             <option key={i} value={option.value}>
+//                               {option.label}
+//                             </option>
+//                           </>
+//                         );
+//                       })}
+//                     </Field>
+//                   </label>
+//                 </Grid>
+//               </Grid>
+//             );
+//           })}
+//           <button type="submit">Submit</button>
+//       </form>

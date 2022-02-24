ea0a2ab8b2bc7a73aa5e749d3409020775092a29
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2022-02-23 23:28:04 -0800

    avail. model, router, controller

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -1,19 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Availability = require('../models/Availability');
 
-// @route POST /availability/{sitterId}
-// @access Private
-exports.createAvailability = asyncHandler(async (req, res, next) => {
-  const loggedInUser = req.user.id ?? false;
-
-  if (!loggedInUser) {
-    res.status(500);
-    throw new Error("Error finding user");
-  }
-
-  const availability = await Availability.create({ ...req.body, sitterId: loggedInUser });
-  res.status(201).json({ success: { availability } });
-});
 
 // @route GET /availability/{recordId}
 // @access Private
@@ -66,3 +53,74 @@ exports.deleteAvailability = asyncHandler(async (req, res, next) => {
   const updated = await Availability.findOneAndDelete(req.params.recordId);
   res.status(201).send();
 });
+// @route GET /availability/?monday[from]={900}&monday[to]={1300}, etc
+// @desc Get available sitter list based on required customer availability from query
+// @access Private
+exports.getAvailableSitters = asyncHandler(async (req, res, next) => {
+  const query = { ...req.query };
+
+  const queryHasAvailabilities = query.hasOwnProperty('monday')
+    || query.hasOwnProperty('tuesday')
+    || query.hasOwnProperty('wednesday')
+    || query.hasOwnProperty('thursday')
+    || query.hasOwnProperty('friday');
+
+  
+  if (!queryHasAvailabilities) {
+    res.status(400);
+    throw new Error("Availability required in query string");
+  }
+
+  const specTime = {
+    monday: query.monday ?? { from: 0, to: 0 },
+    tuesday: query.tuesday ?? { from: 0, to: 0 },
+    wednesday: query.wednesday ?? { from: 0, to: 0},
+    thursday: query.thursday ?? { from: 0, to: 0 },
+    friday: query.friday ?? { from: 0, to: 0 },
+    saturday: query.saturday ?? { from: 0, to: 0 },
+    sunday: query.sunday ?? { from: 0, to: 0 },
+  };
+
+  const availableSitters = await Availability.find({
+    'monday.from': { $lte: specTime.monday.from },
+    'monday.to': { $gte: specTime.monday.to },
+    //
+    'tuesday.from': { $lte: specTime.tuesday.from },
+    'tuesday.to': { $gte: specTime.tuesday.to },
+    //
+    'wednesday.from': { $lte: specTime.wednesday.from },
+    'wednesday.to': { $gte: specTime.wednesday.to },
+    //
+    'thursday.from': { $lte: specTime.thursday.from },
+    'thursday.to': { $gte: specTime.thursday.to },
+    //
+    'friday.from': { $lte: specTime.friday.from },
+    'friday.to': { $gte: specTime.friday.to },
+    //
+    'saturday.from': { $lte: specTime.saturday.from },
+    'saturday.to': { $gte: specTime.saturday.to },
+    //
+    'sunday.from': { $lte: specTime.sunday.from },
+    'sunday.to': { $gte: specTime.sunday.to },
+  });
+
+  res.status(200).json({
+    success: {
+      data: availableSitters ?? []
+    }
+  });
+});
+
+// @route POST /availability/{sitterId}
+// @route POST /availability/{sitterId}
+// @access Private
+exports.createAvailability = asyncHandler(async (req, res, next) => {
+  const loggedInUser = req.user.id ?? false;
+
+  if (!loggedInUser) {
+    res.status(404);
+    throw new Error("Error finding user");
+  }
+
+  const availability = await Availability.create({ ...req.body, sitterId: loggedInUser });
+  res.status(201).json({ success: { availability } });
--- server/routes/availability.js
+++ server/routes/availability.js
@@ -13,5 +13,9 @@ router.route('').post(protect, createAvailability);
 router.route('/:recordId').get(protect, getAvailability);
 router.route('/:recordId').put(protect, updateAvailability);
 router.route('/:recordId').delete(protect, deleteAvailability);
+const { getAvailableSitters, createAvailability } = require('../controllers/availability');
+
+router.route('').get(protect, getAvailableSitters);
+router.route('/:sitterId').post(protect, createAvailability);
 
 module.exports = router;

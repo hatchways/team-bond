9be6b8b7e12fcd81747504fb58cebc49a4b62d1f
ajkatom <contact@alonadelson.com>
2022-03-05 17:31:52 -0500

    most controllers done modified models

---
+++ server/controllers/messages.js
@@ -0,0 +1,43 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+const Message = require('../models/Message');
+const Conversation = require('../models/Conversation');
+
+
+const createConversation = async (participants) => await Conversation.create(participants);
+
+const createMessage = async (message) => {
+  const { conversationId } = message;
+  const { senderId, recipientId } = message;
+  if (!conversationId) {
+    participants = { sender: senderId, recipient: recipientId };
+    const conversation = await createConversation(participants);
+    conversationId = conversation.id;
+  }
+  const message = await Message.create({ ...req.body, conversationId });
+  if (!message) throw new Error('Message format invalid');
+  return message;
+};
+
+exports.sendMessage = asyncHandler(async (req, res, next) => {
+  const message = await createMessage(req.body);
+
+});
+
+exports.findAllConversationMessages = asyncHandler(async (req, res, next) => {
+  const conversationId = req.body.conversationId;
+  const messages = Message.find({ conversationId });
+  res.send(messages);
+});
+
+exports.findAllConversations = asyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+  const conversations = await Conversation.find({
+    $or: [
+      { sender: id },
+      { recipient: id }
+    ]
+  });
+  const messages = conversations.map(async conversation => await Message.find({ conversationId: conversation.id }));
+  res.send(conversations, messages);
+});
--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const conversationSchema = new mongoose.Schema({
-  participants: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
-    }
-  ],
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+
 });
 
 module.exports = Conversation = mongoose.model('Conversation', conversationSchema);

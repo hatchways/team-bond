44b1aae04cfc6abe737e854d82bbd4bb0ff2901b
wkonit <98362157+wkonit@users.noreply.github.com>
2022-03-05 04:45:05 -0500

    Updated ManageBookings PR 3-5-2022

--- client/src/pages/ManageBookings/CalenderView.tsx
+++ client/src/pages/ManageBookings/CalenderView.tsx
@@ -1,49 +1,48 @@
 import React, {useState} from 'react';
-import useStyles from './useStyles'
 import { takeMonth }from './Calender'
-import { Button, Grid, IconButton, Paper } from '@mui/material'
-import {format, isSameMonth, isSameDay, addMonths, subMonths} from 'date-fns'
+import { Grid, IconButton, Paper } from '@mui/material'
+import {format, isSameMonth, isSameDay, addMonths, subMonths, isThisMonth} from 'date-fns'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const CalenderView = () => {
-    const classes = useStyles();
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const data = takeMonth(selectedDate)();
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const freeDay = new Date(); // Change this variable for days available for bookings. Currently set to today's date
+    const data = takeMonth(currentDate)();
 
     function dayColor(day:Date){
-      if(!isSameMonth(day, selectedDate)) return '#CCCCCC';
-      // if(isSameDay(day, selectedDate)) return '#FFFFFF';
+      if(!isSameMonth(day, currentDate)) return '#CCCCCC';
+      if(isSameDay(day, freeDay)) return '#FFFFFF';
       return '#000000';
     }
     function dayBGColor(day:Date){
-      if(!isSameMonth(day, selectedDate)) return '#FFFFFF';
-      // if(isSameDay(day, selectedDate)) return '#f14140';
+      if(!isSameMonth(day, currentDate)) return '#FFFFFF';
+      if(isSameDay(day, freeDay)) return '#f14140';
       return '#FFFFFF';
     }
     function nextMonth(){
-      setSelectedDate(addMonths(selectedDate,1))
-    };
+      setCurrentDate(addMonths(currentDate,1))
+    }
     function prevMonth(){
-      setSelectedDate(subMonths(selectedDate,1))
+      setCurrentDate(subMonths(currentDate,1))
     }
     return (
             <Grid container sx={{paddingLeft:10}} >
-                <Paper sx={{width: 550, height: 400, display: "flex", flexDirection:"column"}}>
+                <Paper elevation={2} sx={{width: 550, height: 400, display: "flex", flexDirection: "column"}}>
                   <Grid sx={{  color: "#f14140", fontSize: 20, fontWeight: 400, fontFamily: "Roboto", margin: 3, paddingLeft:2}}>
-                    <Grid item sx={{ float:"left"}}>
+                    <Grid item sx={{ float: "left"}}>
                       <IconButton onClick={() => prevMonth()}>
                           <ArrowBackIosNewIcon/>
                       </IconButton>
                     </Grid>
-                    <Grid item sx={{float:"right"}}>
+                    <Grid item sx={{float: "right"}}>
                       <IconButton onClick={() => nextMonth()}>
                         <ArrowForwardIosIcon/>
                       </IconButton>
                     </Grid>
                     <Grid item sx={{display: "flex", justifyContent: "center"}}>
-                      {format(selectedDate, "MMMM")}{" "}
-                      {format(selectedDate, "yyyy")}
+                      {format(currentDate, "MMMM")}{" "}
+                      {format(currentDate, "yyyy")}
                     </Grid>
                   </Grid>
                   <Grid container columns={7} sx ={{height:200}}> 
@@ -51,15 +50,29 @@ const CalenderView = () => {
                       <Grid item container
                         columns={1}
                         key={week.toString()} 
-                        sx={{ display:"flex", justifyContent: "center", paddingLeft:2}}
+                        sx={{ display: "flex", alignItems: "center", justifyContent: "center", paddingLeft:2}}
                       >
                         {week.map((day) => (
                           <Grid item 
                             key={day.toString()}
-                            onClick={() => setSelectedDate(day)}
-                            sx={{ padding:1,  marginLeft: 2, marginRight: 2, marginTop: 1, marginBottom: 1, color: dayColor(day), borderRadius: "50%", bgcolor: dayBGColor(day), fontWeight: 600, fontFamily: "Roboto", fontSize:14}}
+                            sx={{
+                              width: "35px", 
+                              height: "35px", 
+                              padding:1,  
+                              marginLeft: 2, 
+                              marginRight: 2, 
+                              marginTop: 1, 
+                              marginBottom: 1, 
+                              color: dayColor(day), 
+                              borderRadius: "50%", 
+                              bgcolor: dayBGColor(day), 
+                              fontWeight: 600, 
+                              fontFamily: "Roboto", 
+                              fontSize:14, 
+                              alignItems: "center", 
+                              justifyContent: "center"}}
                           >
-                            {format(day, "dd")}
+                            {format(day, "d")}
                           </Grid>
                         ))}
                         </Grid>

feac9a2ac96c39f6899c274add7c8ab64d610a03
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2022-03-07 21:34:07 -0800

    apply changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,14 @@ import './index.css';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './themes/theme';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
 import { Navbar } from './components/Navbar/Navbar';
-import { allRoutes } from './routes/app';
+import AppRouter from './routes/AppRoutes';
 
 function App(): JSX.Element {
-  const routes = allRoutes.map((route) => {
-    return <Route key={route.route} path={route.route} component={route.page} />;
-  });
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -22,7 +19,7 @@ function App(): JSX.Element {
             <SocketProvider>
               <CssBaseline />
               <Navbar />
-              <Switch>{routes}</Switch>
+              <AppRouter />
             </SocketProvider>
           </AuthProvider>
         </SnackBarProvider>
